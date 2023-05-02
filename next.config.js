3adeb299d2273395115cd0ce905a9251c7de625d
ewbyf <107148516+ewbyf@users.noreply.github.com>
2023-05-01 22:49:47 -0500

    config change

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   reactStrictMode: true,
   async redirects() {
@@ -10,6 +23,12 @@ const nextConfig = {
       },
     ];
   },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'https://chatterbox.imgix.net',
+  }
 }
 
 module.exports = nextConfig
